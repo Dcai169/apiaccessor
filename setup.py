317b65927e04d1f61844d5d199b0c84ad06e5a31
MrTrainCow <legoguy145@gmail.com>
2018-12-09 14:10:27 -0500

    updated to 0.0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='apiaccessor',
-    version='0.0.1.3',
+    version='0.0.1.4',
     author='Daniel Cai',
     author_email='',
     description='A module to access API\'s using OAuth2 or Header-Key authentication.',
